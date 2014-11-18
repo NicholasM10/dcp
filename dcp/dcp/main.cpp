ef0cbc01d8b1164e3f3820a26ec5da31ad03839f
Nika Mchedlidze <ndplay@ymail.com>
2014-11-19 01:31:03 +0400

    Messed with code, pending fix.

--- dcp/dcp/main.cpp
+++ dcp/dcp/main.cpp
@@ -1,20 +1,21 @@
 #include <iostream>
 #include "ConsoleColor.h"
 #include <string>
+
 int sChoice();
 void beepitfgt();
 
 using namespace std;
 
-string s;
-string s1;
+string s; // string 0
+string s1; // string 1
 
+boolean helpUser = false;
 int main()
 {
 	cout << green << "*** Welcome to  Dumb Console Program 0.0.0, please, type 'commands' for help. ***" << endl;
-	cin >> s;
+	cin >> s; 
 	sChoice();
-	cin >> s1;
 }
 
 int sChoice()
@@ -23,14 +24,18 @@ int sChoice()
 	{
 		if (s == "commands" || s == "command" || s == "cmd" || s == "help" || s == "HELP ME RIGHT NOW!" || s == "heeeeeeeeeeeeeelp")
 		{
-
-			cout << green << "COMMANDS: calculator, beep" << endl;
-
+			helpUser = true;
+			cout << green << "COMMANDS: calculator, beep" << endl; // Ignore calculator for now.
 		}
-		if (s == "beep") // Needs to be worked on.
+		else if (s == "beep" && helpUser == true)
 		{
+			while (helpUser == true)
+			{
+				cin >> s1;
+			}
 			for (int beepCounter = 0; beepCounter != 2; beepCounter++){
 				cout << "\a" << endl;
+				break;
 			}
 		}
 		else 

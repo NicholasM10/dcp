8bb199ec9218258d2513437b63da1e24764e8ff0
Nika Mchedlidze <ndplay@ymail.com>
2014-11-28 01:03:17 +0400

    Fixed various bugs.
    still pretty bugged.

--- dcp/dcp/main.cpp
+++ dcp/dcp/main.cpp
@@ -4,13 +4,12 @@
 
 int sChoice();
 void beepitfgt();
-
 using namespace std;
 
 string s; // string 0
 string s1; // string 1
 
-boolean helpUser = false;
+bool helpUser = true;
 int main()
 {
 	cout << green << "*** Welcome to  Dumb Console Program 0.0.0, please, type 'commands' for help. ***" << endl;
@@ -20,39 +19,34 @@ int main()
 
 int sChoice()
 {
-	
 	{
 		if (s == "commands" || s == "command" || s == "cmd" || s == "help" || s == "HELP ME RIGHT NOW!" || s == "heeeeeeeeeeeeeelp")
 		{
 			helpUser = true;
 			cout << green << "COMMANDS: calculator, beep" << endl; // Ignore calculator for now.
 		}
-		else if (s == "beep" && helpUser == true)
+		else if (s == "beep" ||s == "beepit" ||s == "Beep" ||s == "BEEP" && helpUser == true)
 		{
-			while (helpUser == true)
-			{
-				cin >> s1;
-			}
-			for (int beepCounter = 0; beepCounter != 2; beepCounter++){
+			for (int beepCounter = 0; beepCounter <= 2; beepCounter++){
 				cout << "\a" << endl;
-				break;
+				//break;
+				cin >> s;
 			}
 		}
 		else 
 		{
 			cout << red << "What?!" << endl;
-	
-			while (true)
-			{
-				system("PAUSE");
-			}
 			return 0;
+		}
+		while (true)
+		{
+			system("PAUSE");
 		}
 	}
 }
-
-void beepitfgt()
+/*
+void beepit()
 {
 	
 }
-
+*/
